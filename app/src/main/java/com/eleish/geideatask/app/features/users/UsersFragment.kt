8cc41f8e5e0396dffb990d05e5a96d60e91efeca
eleish <mohamed.a.eleish@gmail.com>
2022-08-19 14:20:34 +0200

    Setup nav

---
+++ app/src/main/java/com/eleish/geideatask/app/features/users/UsersFragment.kt
@@ -0,0 +1,7 @@
+package com.eleish.geideatask.app.features.users
+
+import androidx.fragment.app.Fragment
+import com.eleish.geideatask.R
+
+class UsersFragment: Fragment(R.layout.fragment_users) {
+}
